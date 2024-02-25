5441320221b5b0f4fd33b3964ad36c89ed93b9ec
Gargamel-l <belyton2002@yandex.ru>
2024-02-25 18:37:20 +0300

    #Adding age check and filtration to another collection suitable users(they must be older then 25years and younger then 40)

--- PythonTestingApplication/PythonTestingApplication/PythonTestingApplication.py
+++ PythonTestingApplication/PythonTestingApplication/PythonTestingApplication.py
@@ -1,19 +1,41 @@
-﻿import pymongo
-
+﻿from genericpath import exists
 import tkinter as tk
 from pymongo import MongoClient
 
+
 def insert_data():
     name = entry_name.get()
-    age = entry_age.get()
+    age = int(entry_age.get())
+    if int(age) < 18:
+        status_label.config(text="You're too young!")
+    else:
+        db.my_collection.insert_one({'name': name, 'age': age})
+        status_label.config(text="Data inserted successfully")
+        
+def cv_check():
+    # Получаем подходящих пользователей из коллекции my_collection
+    suitable_users_cursor = db.my_collection.find({'age': {"$gte": int(25), "$lte": int(40)}})
     
-    db.my_collection.insert_one({'name': name, 'age': age})
-    status_label.config(text="Data inserted successfully")
-
+    # Создаем список подходящих пользователей
+    suitable_users = list(suitable_users_cursor)
+    
+    if suitable_users:
+        # Вставляем подходящих пользователей в коллекцию suitable_users
+        db.suitable_users.insert_many(suitable_users)
+        info_label.config(text="All suitable users were sorted")
+    else:
+        info_label.config(text="No suitable users found")
+        
 
 # Подключение к MongoDB
 client = MongoClient('mongodb://localhost:27017/')
 db = client['mydatabase']
+
+
+if exists: db.drop_collection('suitable_users')
+db.create_collection('suitable_users')
+print('collection created')
+
 
 # Создание графического интерфейса с использованием Tkinter
 root = tk.Tk()
@@ -36,4 +58,11 @@ insert_button.grid(row=2, columnspan=2)
 status_label = tk.Label(root, text="")
 status_label.grid(row=3, columnspan=2)
 
+insert_button = tk.Button(root, text="Find suitable users", command=cv_check)
+insert_button.grid(row=4, columnspan=2)
+
+info_label = tk.Label(root, text="")
+info_label.grid(row=5, columnspan=2)
+
 root.mainloop()
+
